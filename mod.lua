70f26103dee6a476b05ea1902d8391a8b0612be3
Dylan7654 <dylan.kreuzinger@gmail.com>
2025-01-04 23:14:05 +0100

    V2.0

--- mod.lua
+++ mod.lua
@@ -1,27 +1,93 @@
 ﻿function data()
 return {
 	info = {
-		minorVersion = 1,
+		minorVersion = 0,
 		severityAdd = "NONE",
 		severityRemove = "WARNING",
 		name = _("mod_name"),
 		description = _("mod_desc"),
 		authors = {
-		    {
-		        name = "ModWerkstatt",
-		        role = "CREATOR",
-		    },
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",
+			},
 		},
-		tags = { "europe", "electric", "elektro", "deutschland", "germany", "lokomotive", "bundesbahn", "deutsche bahn" },
+		tags = { "europe", "locomotive", "bundesbahn", "deutsche bahn", "electric", },
 		minGameVersion = 0,
 		dependencies = { },
 		url = { "" },
+        params = {
+			{
+				key = "br151_fake",
+				name = _("br151_fake"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_br151_desc"),
+				defaultIndex = 0,
+			},
+			{
+				key = "br151_vorspann",
+				name = _("br151_vorspann"),
+				values = { "No", "Yes", },
+				tooltip = _("option_vorspann_br151_desc"),
+				defaultIndex = 0,
+			},
+			{
+				key = "br151_privatbahn",
+				name = _("br151_privatbahn"),
+				values = { "No", "Yes", },
+				tooltip = _("option_br151_privatbahn_desc"),				
+				defaultIndex = 1,
+			},
+        },
 	},
 	options = {
 	},
-
-	runFn = function (settings)
-
+	runFn = function (settings, modParams)
+		
+		local vorspannFilter = function(fileName, data)		
+			if data.metadata.transportVehicle and data.metadata.br151 and data.metadata.br151.vorspann == true then				
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2
+				--return false
+			end	
+			--return true
+			return data
+		end 
+		
+		local fakeFilter = function(fileName, data)		
+			if data.metadata.transportVehicle and data.metadata.br151 and data.metadata.br151.fake == true then		
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2				
+			end				
+			return data
+		end 
+		
+		local privatbahnFilter = function(fileName, data)		
+			if data.metadata.transportVehicle and data.metadata.br151 and data.metadata.br151.privatbahn == true then			
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2				
+			end				
+			return data
+		end 
+		
+		if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]					
+			if params["br151_vorspann"] == 0 then				
+				--addFileFilter("model/transportVehicle", vorspannFilter)	
+				addModifier("loadModel", vorspannFilter)
+			end
+			if params["br151_fake"] == 0 then				
+				addModifier("loadModel", fakeFilter)
+			end
+			if params["br151_privatbahn"] == 0 then				
+				addModifier("loadModel", privatbahnFilter)
+			end
+			
+		else
+			--addFileFilter("model/transportVehicle", vorspannFilter)			
+			addModifier("loadModel", fakeFilter)
+			addModifier("loadModel", privatbahnFilter)
+		end			
 	end
-	}
+}
 end
