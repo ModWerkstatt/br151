2dbc62866575bffe0982575a14ae1d28d70cadd1
Florian Thiel <flth@flth.de>
2025-02-06 20:42:35 +0100

    Strings angepasst und übersetzt

--- strings.lua
+++ strings.lua
@@ -6,14 +6,14 @@ return {
 		["mod_desc"] = "The 151 series is a series of heavy freight locomotives. As the successor to the 150 series, they pull heavier trains at higher speeds. Built from 1972, the housing borrowed from the BR 103 and many components from the tried-and-tested standard series. The locomotives were the heaviest six-axle freight locomotives of the Deutsche Bundesbahn and are the only six-axled engines which are still in service in Germany.",
 
 		["br151_menu"] = "DB Class 151",
-		
+
         ["br151_vorspann_menu"] = "Class 151 Lead",
-		
+
         ["br151_fake_menu"] = "Class 151 Fake",
-		
+
         ['vorspann'] = 'Lead - ',
         ['fake'] = 'Fake - ',
-		
+
         ["br151_vorspann"] = "Active lead engines?",
         ["option_vorspann_br151_desc"] = "Active the lead engines (bottle green front panto raised)?",
 
@@ -21,19 +21,35 @@ return {
         ["option_fake_br151_desc"] = "Active the fake engines (pantos down, no light, no driver)?",
 
 	    ["br151_privatbahn"] = "Activate private liveries?",
-	    ["option_privatbahn_br151_desc"] = "Should the private liveries of all classes are activated in the game??",
-				
-		["br151_co_name"] = "Class 151 chromium oxide green (1974)",
-		
-		["br151_ob_name"] = "Class 151 turquoise/cream (1975)",
-		
-		["br151_or_name"] = "Class 151 orient red (1987)",
-		
-		["br151_vr_name"] = "Class 151 traffic red DB AG (1998)",
-		
-		
-		
-		
+	    ["option_privatbahn_br151_desc"] = "Should the private liveries of all classes are activated in the game?",
+
+		["br151_co_name"] = 	"Class 151 chrome oxide green (1968)",
+        ["br151_co2_name"] = 	"Class 151 chrome oxide green DB emblem red (1987)",
+        ["br151_co3_name"] = 	"Class 151 chrome oxide green DB AG (1994)",
+        ["br151_ob_name"] = 	"Class 151 turquoise/cream (1978)",
+        ["br151_ob2_name"] = 	"Class 151 turquoise/cream DB emblem red (1987)",
+        ["br151_ob3_name"] = 	"Class 151 turquoise/cream DB AG (1994)",
+        ["br151_or_name"] = 	"Class 151 orient red (1987)",
+        ["br151_or2_name"] = 	"Class 151 orient red 2 (1987)",
+        ["br151_or3_name"] = 	"Class 151 orient red DB AG (1994)",
+        ["br151_or4_name"] = 	"Class 151 orient red DB AG mit UIC Nummer (2009)",
+        ["br151_or5_name"] = 	"Class 151 orient red Ralion Logistics (2007)",
+        ["br151_vr_name"] = 	"Class 151 traffic red DB AG (1998)",
+        ["br151_vr2_name"] = 	"Class 151 traffic red DB AG w/ UIC number (2009)",
+        ["br151_vr3_name"] = 	"Class 151 traffic red DB Cargo (1999)",
+        ["br151_vr4_name"] = 	"Class 151 traffic red Ralion (2003)",
+        ["br151_vr5_name"] = 	"Class 151 traffic red Ralion Logistics (2007)",
+
+        ["br151_byb_name"] = 	"Class 151 chrome oxide green Bayernbahn (2018)",
+
+        ["br151_rbh_name"] = 	"Class 151 traffic red RBH (2012)",
+        ["br151_rbh2_name"] = 	"Class 151 blue/white aluminium RBH (2015)",
+
+        ["br151_rpool_name"] = 	"Class 151 traffic red Railpool w/o logo (2017)",
+        ["br151_rpool2_name"] = "Class 151 traffic red Railpool w/ logo (2017)",
+
+        ["br151_sri_name"] = 	"Class 151 chrome oxide green SRI (2012)",
+
 	},
 	de = {
 
@@ -41,14 +57,14 @@ return {
 		["mod_desc"] = "Die Baureihe 151 ist eine Baureihe von schweren Güterzuglokomotiven. Als Nachfolger für die Baureihe 150 ziehen sie schwerere Züge mit höheren Geschwindigkeiten. Gebaut ab 1972 hat das Gehäuse Anleihen aus der BR 103 und viele Komponenten aus den bewährten Einheitsbaureihen. Die Loks waren die schwersten sechsachsigen Güterzuglokomotiven der Deutschen Bundesbahn und sind die letzten Sechsachser, die regulär im Einsatz sind.",
 
         ["br151_menu"] = "BR 151",
-		
+
         ["br151_vorspann_menu"] = "BR 151 Vorspann",
-		
+
         ["br151_fake_menu"] = "BR 151 Fake",
-		
+
         ['vorspann'] = 'Vorspann - ',
         ['fake'] = 'Fake - ',
-		
+
         ["br151_vorspann"] = "Vorspann-Loks aktivieren?",
         ["option_vorspann_br151_desc"] = "Sollen die Vorspann-Loks (mit vorderem Panto gehoben) aktiviert werden?",
 
@@ -57,37 +73,34 @@ return {
 
 		["br151_privatbahn"] = "Privatbahn Lackierungen aktivieren?",
 		["option_privatbahn_br151_desc"] = "Sollen die Lackierungen der Privatbahnen im Spiel aktiviert werden?",
-				
+
         ["br151_co_name"] = 	"BR 151 chromoxidgrün (1968)",
-        ["br151_co2_name"] = 	"BR 151 chromoxidgrün DB Keks himbeerrot (1987)",
-        ["br151_co3_name"] = 	"BR 151 chromoxidgrün DB AG (1994)",		
+        ["br151_co2_name"] = 	"BR 151 chromoxidgrün DB Keks rot (1987)",
+        ["br151_co3_name"] = 	"BR 151 chromoxidgrün DB AG (1994)",
         ["br151_ob_name"] = 	"BR 151 ozeanblau/beige (1978)",
-        ["br151_ob2_name"] = 	"BR 151 ozeanblau/beige DB Keks himbeerrot (1987)",
-        ["br151_ob3_name"] = 	"BR 151 ozeanblau/beige DB AG (1994)",		
-        ["br151_or_name"] = 	"BR 151 orientrot (1987)",	
+        ["br151_ob2_name"] = 	"BR 151 ozeanblau/beige DB Keks rot (1987)",
+        ["br151_ob3_name"] = 	"BR 151 ozeanblau/beige DB AG (1994)",
+        ["br151_or_name"] = 	"BR 151 orientrot (1987)",
         ["br151_or2_name"] = 	"BR 151 orientrot 2 (1987)",
-        ["br151_or3_name"] = 	"BR 151 orientrot DB AG (1994)",	
-        ["br151_or4_name"] = 	"BR 151 orientrot DB AG mit UIC Nummer (2009)",	
-        ["br151_or5_name"] = 	"BR 151 orientrot Ralion Logistics (2007)",				
+        ["br151_or3_name"] = 	"BR 151 orientrot DB AG (1994)",
+        ["br151_or4_name"] = 	"BR 151 orientrot DB AG mit UIC Nummer (2009)",
+        ["br151_or5_name"] = 	"BR 151 orientrot Ralion Logistics (2007)",
         ["br151_vr_name"] = 	"BR 151 verkehrsrot DB AG (1998)",
         ["br151_vr2_name"] = 	"BR 151 verkehrsrot DB AG mit UIC Nummer (2009)",
         ["br151_vr3_name"] = 	"BR 151 verkehrsrot DB Cargo (1999)",
         ["br151_vr4_name"] = 	"BR 151 verkehrsrot Ralion (2003)",
         ["br151_vr5_name"] = 	"BR 151 verkehrsrot Ralion Logistics (2007)",
-		
+
         ["br151_byb_name"] = 	"BR 151 chromoxidgrün Bayernbahn (2018)",
-		
+
         ["br151_rbh_name"] = 	"BR 151 verkehrsrot RBH (2012)",
-        ["br151_rbh2_name"] = 	"BR 151 Blau/Weißaluminium RBH (2015)",
-		
+        ["br151_rbh2_name"] = 	"BR 151 blau/weißaluminium RBH (2015)",
+
         ["br151_rpool_name"] = 	"BR 151 verkehrsrot Railpool ohne Logo (2017)",
         ["br151_rpool2_name"] = "BR 151 verkehrsrot Railpool mit Logo (2017)",
-		
+
         ["br151_sri_name"] = 	"BR 151 chromoxidgrün SRI (2012)",
-		
-		
-		
-		
+
 	},
 }
 end
