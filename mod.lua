fe986cee6cc9db53afb21150f96183d032d92eac
Florian Thiel <flth@flth.de>
2025-05-04 18:26:20 +0200

    Update auf v2.1

--- mod.lua
+++ mod.lua
@@ -1,7 +1,7 @@
 ﻿function data()
 return {
 	info = {
-		minorVersion = 0,
+		minorVersion = 1,
 		severityAdd = "NONE",
 		severityRemove = "WARNING",
 		name = _("mod_name"),
@@ -35,7 +35,7 @@ return {
 				key = "br151_privatbahn",
 				name = _("br151_privatbahn"),
 				values = { "No", "Yes", },
-				tooltip = _("option_privatbahn_br151_desc"),				
+				tooltip = _("option_privatbahn_br151_desc"),
 				defaultIndex = 1,
 			},
         },
@@ -43,51 +43,51 @@ return {
 	options = {
 	},
 	runFn = function (settings, modParams)
-		
-		local vorspannFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.br151 and data.metadata.br151.vorspann == true then				
+
+		local vorspannFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.br151 and data.metadata.br151.vorspann == true then
 				data.metadata.availability.yearFrom = 1
 				data.metadata.availability.yearTo = 2
 				--return false
-			end	
+			end
 			--return true
 			return data
-		end 
-		
-		local fakeFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.br151 and data.metadata.br151.fake == true then		
+		end
+
+		local fakeFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.br151 and data.metadata.br151.fake == true then
 				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
+				data.metadata.availability.yearTo = 2
+			end
 			return data
-		end 
-		
-		local privatbahnFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.br151 and data.metadata.br151.privatbahn == true then			
+		end
+
+		local privatbahnFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.br151 and data.metadata.br151.privatbahn == true then
 				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
+				data.metadata.availability.yearTo = 2
+			end
 			return data
-		end 
-		
+		end
+
 		if modParams[getCurrentModId()] ~= nil then
-			local params = modParams[getCurrentModId()]					
-			if params["br151_vorspann"] == 0 then				
-				--addFileFilter("model/transportVehicle", vorspannFilter)	
+			local params = modParams[getCurrentModId()]
+			if params["br151_vorspann"] == 0 then
+				--addFileFilter("model/transportVehicle", vorspannFilter)
 				addModifier("loadModel", vorspannFilter)
 			end
-			if params["br151_fake"] == 0 then				
+			if params["br151_fake"] == 0 then
 				addModifier("loadModel", fakeFilter)
 			end
-			if params["br151_privatbahn"] == 0 then				
+			if params["br151_privatbahn"] == 0 then
 				addModifier("loadModel", privatbahnFilter)
 			end
-			
+
 		else
-			--addFileFilter("model/transportVehicle", vorspannFilter)			
+			--addFileFilter("model/transportVehicle", vorspannFilter)
 			addModifier("loadModel", fakeFilter)
 			addModifier("loadModel", privatbahnFilter)
-		end			
+		end
 	end
 }
 end
